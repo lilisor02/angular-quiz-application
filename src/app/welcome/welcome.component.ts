429ae51378013bd2add30bcd83f48b76c5aa2ee7
liliana_gubernu <andreea.liliana02@gmail.com>
2023-09-15 17:09:45 +0300

    commit on Main

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,18 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss']
+})
+export class WelcomeComponent {
+  @ViewChild('name') nameKey!: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  startQuiz() {
+    localStorage.setItem("name", this.nameKey.nativeElement.value);
+  }
+}
